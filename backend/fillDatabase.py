8e9092679942fdeddbb43c7b64dfed9e2ee11209
Annemarie Brekow <annemariebrekow@gmail.com>
2024-03-18 16:41:54 +0100

    stuff

--- backend/fillDatabase.py
+++ backend/fillDatabase.py
@@ -24,16 +24,18 @@ def getAllTableNames(db) -> list:
 
 dbFile = "D:\\Schule\\LF12\\ls12_Projekt_Heatmap\\HEAT-9\\Heatmap\\backup\\studentDB.db"
 db = "backend/bin/studentDB.db"
+content = evaluateData.provideData()
 #getAllTableNames(dbFile)
 
 #region consts
 tables = ["AllData", "Status", "Abwesenheitsgrund", "Klasse", "Student", "Abwesenheiten"]
 statusEntries = evaluateData.getStatus()
 absenceReasonEntries = evaluateData.getAbwesenheitsgrund()
+classEntries = evaluateData.getClasses()
 #endregion
 
 #region table Queries
-createCSVcopyTbl = "CREATE TABLE AllData (id integer NOT NULL, lastname varchar(30) NOT NULL, firstname varchar(30) NOT NULL, className varchar(10) NOT NULL, start varchar(16) NOT NULL, end varchar(16) NOT NULL, absenceReason char(2), status beschreibung varchar(30) NOT NULL)"
+createCSVcopyTbl = "CREATE TABLE AllData (id integer NOT NULL, lastname varchar(30) NOT NULL, firstname varchar(30) NOT NULL, className varchar(10) NOT NULL, start varchar(16) NOT NULL, end varchar(16) NOT NULL, absenceReason char(2), status varchar(30) NOT NULL)"
 createStatusTbl = "CREATE TABLE Status (id_status_pk integer PRIMARY KEY, beschreibung varchar(30) NOT NULL)"
 createAbsenceReasonsTbl = "CREATE TABLE Abwesenheitsgrund (id_abwesenheitsgrund_pk char(1) PRIMARY KEY, beschreibung varchar(80) NOT NULL)"
 createClassesTbl = "CREATE TABLE Klasse (id_klasse_pk integer PRIMARY KEY, beschreibung varchar(10) NOT NULL)"
@@ -53,30 +55,114 @@ def createTables():
             print(e)
     conn.commit()
     dbCon.disconnect(conn)
+   
+def insertValue(db:str, col:str, val:str):
+    return "INSERT INTO " + db + "(" + col + ") VALUES (" + val + ");"
 
-# createTables()
-
-
-# populate AllData
 def populateAllDataTbl():
-    data = evaluateData.provideData()
-    # TODO 
-#   c.execute(insertValue("Student", "id_student_pk, nachname, vorname, id_klasse_fk", "'" + students[0] + "', '" + splitName[0] + "', '" + splitName[1] + "', '" + str(klasse_) + "'"))
+    conn = dbCon.connect(db)
+    for line in content:
+        try:
+            s = "'" + line[0] + "', '" + line[1] + "', '" + line[2] + "', '" + line[3] + "', '" + line[4] + "', '" + line[5] + "', '" + line[6] + "', '" + line[7] + "'"
+            c = conn.cursor()
+            c.execute(insertValue(tables[0], "id, lastname, firstname, className, start, end, absenceReason, status", s))
+        except Error as e:
+            print(e)
+    conn.commit()
+    dbCon.disconnect(conn)
 
-    pass
+def populateStatus():
+    conn = dbCon.connect(db)
+    for entry in statusEntries:
+        try:
+            c = conn.cursor()
+            c.execute(insertValue(tables[1], "id_status_pk, beschreibung", "NULL, '" + entry + "'"))
+        except Error as e:
+            print(e)
+    conn.commit()
+    dbCon.disconnect(conn)
+    
+def populateAbsenceReason():
+    conn = dbCon.connect(db)
+    for entry in absenceReasonEntries:
+        try:
+            c = conn.cursor()
+            c.execute(insertValue(tables[2], "id_abwesenheitsgrund_pk, beschreibung", "'" + entry[0] + "', '" + entry[1] + "'"))
+        except Error as e:
+            print(e)
+    conn.commit()
+    dbCon.disconnect(conn)
 
-populateAllDataTbl()
+def populateClass():
+    conn = dbCon.connect(db)
+    for entry in classEntries:
+        try:
+            c = conn.cursor()
+            c.execute(insertValue(tables[3], "id_klasse_pk, beschreibung", "NULL , '" + entry + "'"))
+        except Error as e:
+            print(e)
+    conn.commit()
+    dbCon.disconnect(conn)
 
-# beginn und ende concaten 
+def getClassTbl():
+    query = "SELECT * FROM " + tables[3] + ";"
+    conn = dbCon.connect(db)
+    c = conn.cursor()
+    try:
+        c.execute(query)
+        results = c.fetchall()
+    except Error as e:
+        print(e)
+    dbCon.disconnect(conn)
+    table = []
+    for result in results:
+        table.append([result[0], result[1]])
+    return table
 
-# populate Status -> evaluateData.getStatus()
-# populate Abwesenheitsgrund -> evaluateData.getAbwesenheitsgrund()
 
-# getClasses aus AllData (unique)
-# populate Klasse
 
-# getKlassenID -> id_klasse_fk
+
+
+
+
+
+def getClassId(className:str):
+    classes = getClassTbl()
+    for cl in classes:
+        if cl[1] == className:
+            return cl[0]
+        else:
+            return None
+
+# print(getClassTbl()) # [x] - klasse mit [x][0] id und [x][1] beschreibung
+
+# ['haase_mike', 'Haase', 'Mike', 'ZHN 02', '2023.08.30 08:00', '2023.08.30 17:00', 'K', 'entsch.']
+# tables = ["AllData", "Status", "Abwesenheitsgrund", "Klasse", "Student", "Abwesenheiten"]
+
+# "CREATE TABLE Student (id_student_pk varchar(50) PRIMARY KEY, nachname varchar(30) NOT NULL, vorname varchar(30) NOT NULL, id_klasse_fk integer, FOREIGN KEY (id_klasse_fk) REFERENCES Klasse (id_klasse_pk))"
 # populate Student
+
+def populateStudent():
+    conn = dbCon.connect(db)
+    for entry in content:
+        cl = getClassId(entry[3])
+        if cl == None:
+            s = "'" + entry[0] + "' , '" + entry[1] + "' , '" + entry[2] + "' , NULL" # TODO
+        else:
+            s = "'" + entry[0] + "' , '" + entry[1] + "' , '" + entry[2] + "' , " + str(cl)
+        print(s)
+        try:
+            c = conn.cursor()
+            c.execute(insertValue(tables[4], "id_student_pk, nachname, vorname, id_klasse_fk", s))
+        except Error as e:
+            print(e)
+    # conn.commit()
+    dbCon.disconnect(conn)
+
+populateStudent()
+
+
+
 
 # getAbsenceReasonID -> id_abwesenheitsgrund_fk
 # getStatusID -> id_status_fk
@@ -86,9 +172,17 @@ populateAllDataTbl()
 
 
 
+# createTables()
+# populateAllDataTbl()
+# populateStatus()
+# populateAbsenceReason()
+# populateClass()
+    
 
 
-def shootQuery(query):
+
+
+def sendQuery(query):
     conn = dbCon.connect(db)
     try:
         c = conn.cursor()
